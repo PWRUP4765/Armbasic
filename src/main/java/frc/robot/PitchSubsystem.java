bce13f406b0e7daf48c9ab211f3b5dffc391e591
Arnav Karandikar <arnavk@Amits-MacBook-Pro.local>
2023-03-08 16:35:35 -0800

    initial commit

---
+++ src/main/java/frc/robot/PitchSubsystem.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+public class PitchSubsystem extends SubsystemBase {
+    private final TalonSRX m_motordrive_pitch = new TalonSRX(5);
+    private final DutyCycleEncoder m_encoder_pitch = new DutyCycleEncoder (0); // pitch
+    private final double upper_limit = 0.95;
+    private final double lower_limit = 0.55;
+    
+    public void move(double axis){
+        double encoder_pitch_value = m_encoder_pitch.get();
+        if (encoder_pitch_value>0.55 && encoder_pitch_value<0.95)
+        m_motordrive_pitch.set(ControlMode.PercentOutput, axis*0.5);
+    }
+}
